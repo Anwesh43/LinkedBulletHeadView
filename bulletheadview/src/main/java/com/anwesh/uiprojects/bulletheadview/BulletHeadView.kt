2746263c117a50724bfa685af689ed146cbbcec3
Anwesh43 <anweshthecool0@gmail.com>
2019-04-16 09:26:09 +0530

    adding BulletHeadNode class

--- bulletheadview/src/main/java/com/anwesh/uiprojects/bulletheadview/BulletHeadView.kt
+++ bulletheadview/src/main/java/com/anwesh/uiprojects/bulletheadview/BulletHeadView.kt
@@ -114,8 +114,52 @@ class BulletHeadView(ctx : Context) : View(ctx) {
 
         fun stop() {
             if (animated) {
-                animated = false 
+                animated = false
             }
+        }
+    }
+
+    data class BHNode(var i : Int, val state : State = State()) {
+
+        private var next : BHNode? = null
+        private var prev : BHNode? = null
+
+        fun update(cb : (Int, Float) -> Unit) {
+            state.update {
+                cb(i, it)
+            }
+        }
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < nodes - 1) {
+                next = BHNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawBHNode(i, state.scale, paint)
+            next?.draw(canvas, paint)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : BHNode {
+            var curr : BHNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
         }
     }
 }
